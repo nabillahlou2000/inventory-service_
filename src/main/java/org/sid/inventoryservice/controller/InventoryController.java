6794421d8535716011db967e0527eddbe777e39f
unknown <nabillahlou138@gmail.com>
2023-01-12 18:17:17 +0100

    Initial commit

---
+++ src/main/java/org/sid/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,22 @@
+package org.sid.inventoryservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.sid.inventoryservice.service.InventoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+        return inventoryService.isInStock(skuCode);
+
+
+    }
+}
